4ef285b7d7049c33601faaf4e8a1ea2efd41a00a
Leo <lloustalot@artfx.fr>
2022-11-04 10:52:47 +0100

    Area Discovered text

---
+++ Assets/Scripts/EnteredAreaTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnteredAreaTrigger : MonoBehaviour
+{
+    bool hasBeenActivated;
+    public string areaName;
+
+    private void Awake()
+    {
+        hasBeenActivated = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasBeenActivated && other.gameObject.tag == "Player")
+        {
+            hasBeenActivated = true;
+            DiscoveryPopUp.EnteredNewArea.Invoke(areaName);
+        }
+    }
+}
