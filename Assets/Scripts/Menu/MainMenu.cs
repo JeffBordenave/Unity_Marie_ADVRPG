14d4abcf031b46a933de8f806e8d5f4d88842c0b
JeffBordenave <gbordenave@artfx.fr>
2022-11-06 16:24:38 +0100

    what in the fuck am I doing rn

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject HUD;
+    public GameObject Player;
+
+    private void Start()
+    {
+        HUD.SetActive(false);
+        Time.timeScale = 0;
+        Player.SetActive(false);
+    }
+
+    public void OnPlayButton()
+    {
+        HUD.SetActive(true);
+        Player.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
