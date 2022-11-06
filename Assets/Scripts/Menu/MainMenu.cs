17f1714af0ab0693a5b9aaeada6be4bf39e6753f
JeffBordenave <gbordenave@artfx.fr>
2022-11-06 17:10:18 +0100

    built light

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,20 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject HUD;
-    public GameObject Player;
+    //public GameObject HUD;
+    //public GameObject Player;
 
-    private void Start()
-    {
-        HUD.SetActive(false);
-        Time.timeScale = 0;
-        Player.SetActive(false);
-    }
+    //private void Start()
+    //{
+    //    HUD.SetActive(false);
+    //    Time.timeScale = 0;
+    //    Player.SetActive(false);
+    //}
 
     public void OnPlayButton()
     {
-        HUD.SetActive(true);
-        Player.SetActive(true);
-        Time.timeScale = 1;
+        //HUD.SetActive(true);
+        //Player.SetActive(true);
+        SceneManager.LoadScene(1);
     }
 }
